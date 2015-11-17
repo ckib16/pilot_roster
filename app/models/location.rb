e0c01e7d63f088f745cb57159e10b594c5620f54
ckib16 <ckib16@gmail.com>
2015-11-17 14:42:31 -0500

    Debugging Heroku
    - comment out scope in location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ActiveRecord::Base
   validates :base, :unit, :country, :pascode, :billets, :emblem_url, presence: true
   validates :billets, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
-  default_scope { order('base ASC') }
+  # default_scope { order('base ASC') }
 
   has_many :users
 
