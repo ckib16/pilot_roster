c679e229a753bd62ae6d66f6ce0d2d73a99d1eb0
ckib16 <ckib16@gmail.com>
2015-11-17 20:25:21 -0500

    Basic User#index and #show
    - added controller, #index and #show
    - added attributes to users table
    - migrated changes

--- db/migrate/20151115030731_devise_create_users.rb
+++ db/migrate/20151115030731_devise_create_users.rb
@@ -4,6 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :rank
+      t.string :name
+      t.string :qualification
+      t.integer :hours
+      t.boolean :experienced, default: false
 
       ## Recoverable
       t.string   :reset_password_token
