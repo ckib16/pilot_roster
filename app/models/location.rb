b9ffbeee41970e66c038cc7290ca71f58fd6ecfb
RobertAKARobin <robertgfthomas@gmail.com>
2015-11-24 15:00:05 -0500

    robin's comments

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   validates :base, :unit, :country, :pascode, :billets, presence: true
   validates :billets, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-
+  # Great use of validations and default_scope
   default_scope { order('base ASC') }
 
   has_many :pilots
