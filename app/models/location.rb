34eed975d69d2505fcbdb39f0f4ad1467a358457
ckib16 <ckib16@gmail.com>
2015-12-05 18:46:37 -0500

    Added rubocop + blank factories & spec files

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,11 +1,10 @@
 class Location < ActiveRecord::Base
   validates :base, :unit, :country, :pascode, :billets, presence: true
-  validates :billets, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :billets, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   # Great use of validations and default_scope
   default_scope { order('base ASC') }
 
   has_many :pilots
-
 
   # future use variables to breakout locations by country
   # @oconus = Location.where(oconus: true)
