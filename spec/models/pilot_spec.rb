20b21dc5bd0221504613c09f771c91a3a341022e
ckib16 <ckib16@gmail.com>
2015-12-06 18:46:27 -0500

    Pilot spec built & passing
    Rspec:
    Model creation & validation done
    - no class or instance methods to test
    
    Model:
    Added validation for avatar_url to model

--- spec/models/pilot_spec.rb
+++ spec/models/pilot_spec.rb
@@ -5,21 +5,51 @@ describe Pilot do
     expect(build(:pilot)).to be_valid
   end
 
-  it "is valid with a location, name, rank, hours, qualification, commision date, adsc and avatar"
+  it "is invalid without a name" do
+    pilot = build(:pilot, name: nil)
+    pilot.valid?
+    expect(pilot.errors[:name]).to include("can't be blank")
+  end
 
-  it "is invalid without a location"
+  it "is invalid without a rank" do
+    pilot = build(:pilot, rank: nil)
+    pilot.valid?
+    expect(pilot.errors[:rank]).to include("can't be blank")
+  end
 
-  it "is invalid without a name"
+  it "is invalid without an hours integer > 0" do
+    pilot = build(:pilot, hours: -1)
+    pilot.valid?
+    expect(pilot.errors[:hours]).to include("must be greater than or equal to 0")
+  end
 
-  it "is invalid without a rank"
+  it "is invalid without an hours inputed as an integer" do
+    pilot = build(:pilot, hours: "abc")
+    pilot.valid?
+    expect(pilot.errors[:hours]).to include("is not a number")
+  end
 
-  it "is invalid without an hours integer > 0"
+  it "is invalid without a qualification" do
+    pilot = build(:pilot, qualification: nil)
+    pilot.valid?
+    expect(pilot.errors[:qualification]).to include("can't be blank")
+  end
 
-  it "is invalid without a qualification"
+  it "is invalid without a commision date" do
+    pilot = build(:pilot, commision_date: nil)
+    pilot.valid?
+    expect(pilot.errors[:commision_date]).to include("can't be blank")
+  end
 
-  it "is invalid without a commision date"
+  it "is invalid without an adsc" do
+    pilot = build(:pilot, adsc: nil)
+    pilot.valid?
+    expect(pilot.errors[:adsc]).to include("can't be blank")
+  end
 
-  it "is invalid without an adsc"
-
-  it "is invalid without an avatar"
+  it "is invalid without an avatar" do
+    pilot = build(:pilot, avatar_url: nil)
+    pilot.valid?
+    expect(pilot.errors[:avatar_url]).to include("can't be blank")
+  end
 end
